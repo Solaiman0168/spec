8b33d5d67e701476f678a5959b5570f8c8189481
Solaiman Hossain <solaiman0168@gmail.com>
2020-09-26 11:45:56 +0600

    db name change

--- admin/config/db_connection.php
+++ admin/config/db_connection.php
@@ -2,8 +2,8 @@
 	
 	$dbname = "spec_spec";
 	$hostname = "localhost";
-	$username = "spec_spec";
-	$password = "asdf@1234";
+	$username = "";
+	$password = "";
 	$con = mysqli_connect($hostname, $username, $password, $dbname );
 	mysqli_set_charset($con, "utf8");
 ?>
